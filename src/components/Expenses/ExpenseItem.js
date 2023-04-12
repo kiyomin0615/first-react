2ac7af7d7bc2653335301ed07e830bd741497d8d
kiyomin0615 <moeday1010@gmail.com>
2023-04-12 13:47:37 +0900

    2. 이벤트와 상태

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,13 +6,22 @@ import "./ExpenseItem.css";
 
 // 프롭(매개변수)을 통해 데이터 전달받기
 function ExpenseItem(props) {
+  // title is a getter
+  // setTitle is a setter
+  const [title, setTitle] = useState(props.title); // 상태 초기화
+
+  function clickHandler() {
+    setTitle("상태 변경!");
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount} 원</div>
       </div>
+      <button onClick={clickHandler}>수정</button>
     </Card>
   );
 }
